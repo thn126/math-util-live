1abe9038e6f385da11b786153fdd8a040402845c
thn126 <thiennhse162041@fpt.edu.vn>
2022-09-22 11:22:21 +0700

    Lan thu 2 lm chuyen ay - quen r

--- src/com/fu/mathutil/MathUtilLive.java
+++ src/com/fu/mathutil/MathUtilLive.java
@@ -17,11 +17,14 @@ public class MathUtilLive {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("excpected: 5! = 120; actual: "+MathUtil.getFactorial(5));
-        System.out.println("excpected: 3! = 6; actual: "+MathUtil.getFactorial(3));
         System.out.println("excpected: 6! = 720; actual: "+MathUtil.getFactorial(6));
         System.out.println("excpected: 0! = 1; actual: "+MathUtil.getFactorial(0));
         
         //MathUtil.getFactorial(-5); nem exception
+        
+        //Them code sau lan dau tien lm chuyen ay -code len server 11:20 AM 22/9/2022
+        System.out.println("excpected: 1! = 1; actual: "+MathUtil.getFactorial(1));
+        System.out.println("excpected: 3! = 6; actual: "+MathUtil.getFactorial(3));
     }
     
 }
